fea7b4799c38390ba931f02e3dc4a973d03fe514
Matheus Marques <dev.mmarques@gmail.com>
2025-06-17 01:31:25 -0300

    parte 3

---
+++ matemática.py
@@ -0,0 +1,22 @@
+# Funções builtin
+
+valores = [2,4,6,-8,0,12,32]
+print(max(valores))
+print(min(valores))
+a = -21
+b = 23
+print(abs(a)) # mostrará o valor absoluto de a - tipo o módulo 
+print(pow(a,3)) # pow = elevado a 
+
+c = 2.343565
+print(round(c,3)) # função round arredonda o número designado de acordo com as casas decimais indicadas 
+
+
+import math 
+
+x = 18
+y = 100 
+
+raiz_quadrada = math.sqrt(y)
+print(math.ceil(raiz_quadrada)) # arredonda número para cima 
+print(math.floor(raiz_quadrada)) # arredonda número para baixo 
--- tuplas.py
+++ tuplas.py
@@ -1,1 +1,31 @@
 # as tuplas são imutáveis - após criada o conteúdo da tupla não pode ser modificado 
+
+halogenios = ('F', 'Cl', 'Br', 'I', 'At')
+gases_nobres = ('He','Ne', 'Ar', 'Xe', 'Kr', 'Rn')
+elementos = halogenios + gases_nobres # concatenação de tuplas 
+
+t1 = (5,4,6,4,2,9,8,0,7,3,3,3,3,5,6,7,3,2)
+print(t1.count(3))# conta quantas fezes este número específico aparece 
+# - funcona para string tanbém
+
+print('Cl' in halogenios) # Cl está na tupla halogenio 
+print(sum(t1)) # soma todos os itens da tupla 
+print(min(t1)) # mostra o menor valor da tupla 
+print(max(t1)) # mostra o valor máximo da tupla
+
+# Operações nãos disponiveis em tuplas: .sort() , .append() , reverse(), pop()... 
+# - qualquer operação que mude o conteúdo da tupla
+
+for elemento in elementos: 
+    print(f'Elemento químico: {elemento}')
+
+#criar uma lista apartir de uma tupla
+grupo17 = list(halogenios)
+print(type(grupo17))
+print(grupo17)
+
+# criar uma tupla a partir de uma lista 
+grupo1= ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr']
+alcalinos = tuple(grupo1)
+print(type(alcalinos))
+print(sorted(alcalinos))
